f82bc9341860612db76d72602464c1982f7a9277
SeanRichhh <sean-richards@outlook.com>
2022-05-09 17:22:58 +0100

    Core

--- src/app.js
+++ src/app.js
@@ -2,28 +2,44 @@ import Header from './components/header'
 
 import initialEmails from './data/emails'
 
+import {useState} from "react"
+
 import './styles/app.css'
+
+
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
+
+  const inboxEmails = emails.map((printEmail) => {
+    return(
+      <li className="email">
+        <div className="select">
+          <input className="select-checkbox" type="checkbox" />
+        </div>
+        <div className="star">
+          <input className="star-checkbox" type="checkbox" />
+        </div>
+        <div className="sender">{printEmail.sender}</div>
+        <div className="title">{printEmail.title}</div>
+      </li>
+    )
+  })
+
+  
 
   return (
     <div className="app">
       <Header />
       <nav className="left-menu">
         <ul className="inbox-list">
-          <li
-            className="item active"
-            // onClick={() => {}}
-          >
+          <li className="item active">
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
-          <li
-            className="item"
-            // onClick={() => {}}
-          >
+          <li className="item">
             <span className="label">Starred</span>
             <span className="count">?</span>
           </li>
@@ -34,12 +50,12 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+              
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{inboxEmails}</main>
     </div>
   )
 }
